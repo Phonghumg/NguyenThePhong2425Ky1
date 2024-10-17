8f681223fa93c4adc9b8e34e2af9a0b3a748647d
Phonghumg <nguyenthephong12g@gmail.com>
2024-10-17 19:56:45 +0700

    Create_Project

---
+++ NguyenThePhong506/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenThePhong506.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
